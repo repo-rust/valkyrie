97327fe190a2ab241a775aa7aa863a5fe25ddd54
Maksym Stepanenko <maksym.stepanenko@oracle.com>
2025-11-23 10:56:17 +0200

    Additional features added to dependencies for crossbeam and oneshort channels

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,63 @@
 use std::io;
 
 mod startup_arguments;
+
 use crate::startup_arguments::{Mode, StartupArguments};
 
 mod network;
 use crate::network::dispatcher::run_dispatcher;
 use crate::network::reuse::run_reuseport;
 
+// struct Request {
+//     response: oneshot::Sender<String>,
+//     data: String,
+// }
+
 fn main() -> io::Result<()> {
+    // println!("Main done...");
+
+    // let (queue_sender, queue_receiver) = crossbeam_channel::unbounded::<Request>();
+
+    // thread::spawn(move || {
+    //     for single_request in queue_receiver.iter() {
+    //         println!("received message: {}", single_request.data);
+
+    //         single_request
+    //             .response
+    //             .send(format!("response: {}", single_request.data))
+    //             .expect("Failed to send response for oneshort channel")
+
+    //         // println!("{single_request}");
+    //     }
+    // });
+
+    // let rt = tokio::runtime::Builder::new_current_thread()
+    //     .enable_io()
+    //     .enable_time()
+    //     .build()
+    //     .expect("build runtime");
+
+    // rt.block_on(async move {
+    //     for i in 0..5 {
+    //         let (sender, receiver) = oneshot::channel();
+
+    //         let req = Request {
+    //             response: sender,
+    //             data: format!("Hello-{i}").to_string(),
+    //         };
+
+    //         queue_sender
+    //             .send(req)
+    //             .expect("Failed to send message to channel");
+
+    //         let resp = receiver.await.expect("Await on oneshot receiver failed");
+
+    //         println!("response {}", resp);
+    //     }
+    // });
+
+    // Ok(())
+
     let program_arguments = StartupArguments::parse_args();
 
     println!("[main] Program arguments: {program_arguments:?}");
